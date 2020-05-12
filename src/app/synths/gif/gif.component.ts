d6437b0ad42f8c158f662c1aa71a7ce46a0c0237
alegzandach <alex.deutch@gmail.com>
2020-05-12 01:31:42 -0400

    fixed timing error

--- src/app/synths/gif/gif.component.ts
+++ src/app/synths/gif/gif.component.ts
@@ -33,7 +33,7 @@ var Tone = require('tone/build/Tone');
     public rub;
     public synth;
     public gifCanvas;
-    public gifUrl = 'GIF URL'
+    public gifUrl = ''
     public playing: boolean = false;
 
     @ViewChild('img') img: ElementRef;
@@ -41,9 +41,9 @@ var Tone = require('tone/build/Tone');
 
     public ngAfterViewInit() {
       this.synth = new Tone.PolySynth(4, Tone.Synth).toMaster();
-      this.rub = new SuperGif({gif: this.img.nativeElement, on_change: this.onChange, max_width: window.innerWidth*.95, max_height: window.innerHeight*.95, show_progress_bar: false, auto_play: false});
-      this.rub.load();
-      this.gifCanvas = document.getElementById('jsgif_canvas')
+      //this.rub = new SuperGif({gif: this.img.nativeElement, on_change: this.onChange, max_width: window.innerWidth*.95, max_height: window.innerHeight*.95, show_progress_bar: false, auto_play: false});
+      //this.rub.load();
+      //this.gifCanvas = document.getElementById('jsgif_canvas')
     }
 
     public onResize = () => {
@@ -52,14 +52,21 @@ var Tone = require('tone/build/Tone');
     }
 
     public play = () => {
-      if( this.gifUrl != 'GIF URL'){
-        this.img.nativeElement.attributes[1].nodeValue = this.gifUrl;
-        this.rub = new SuperGif({gif: this.img.nativeElement, on_change: this.onChange, max_width: window.innerWidth*.95, max_height: window.innerHeight*.95, show_progress_bar: false, auto_play: false});
-        this.rub.load();
-        this.gifCanvas = document.getElementById('jsgif_canvas')
-      }
       this.rub.play();
       this.playing = true
+    }
+
+    public load = () => {
+      if( this.gifUrl != ''){
+        if ( this.img.nativeElement.attributes[1].nodeValue ){
+          const element = document.getElementById('canvasContainer');
+          element.parentNode.removeChild(element);
+        }  
+        this.img.nativeElement.attributes[1].nodeValue = this.gifUrl;
+        this.rub = new SuperGif({gif: this.img.nativeElement, on_change: this.onChange, max_width: window.innerWidth*.95, max_height: window.innerHeight*.95, show_progress_bar: false, auto_play: false});
+        this.rub.load(this.play);
+        this.gifCanvas = document.getElementById('jsgif_canvas')
+      }
     }
 
     public pause = () => {
